0443d169a252ba43d76616649c8fa77e0bc5697c
dhimanroyit <dhimanroyict@gmail.com>
2021-12-01 01:52:54 +0600

    update image url path

--- src/resources/businessProfile/businessProfileController.js
+++ src/resources/businessProfile/businessProfileController.js
@@ -2,7 +2,7 @@ const { promisify } = require('util');
 const Profile = require('./businessProfileModel');
 
 const createProfile = async (req, res, next) => {
-  const imgUrl = `${req.protocol}://${req.get('host')}/public/${
+  const imgUrl = `${req.protocol}://${req.get('host')}api/v1/public/upload/${
     req.file.filename
   }`;
   console.log(imgUrl);
