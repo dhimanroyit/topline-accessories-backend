d55239689a7ea769925727689a184eaf69d62fe7
dhimanroyit <dhimanroyit@gmail.com>
2021-12-15 02:44:28 +0600

    feat: add imgae upload functionality

--- src/resources/team/teamControllers.js
+++ src/resources/team/teamControllers.js
@@ -1,10 +1,24 @@
 const Team = require('./teamModel');
 const crudOperations = require('../../utils/crudOperations');
+const { removeUploadFile } = require('../../middleware/upload');
 
 const _team = crudOperations(Team, 'team');
 
 const createTeam = async (req, res, next) => {
-  _team.createOne({ ...req.body, createdBy: req.user._id }, res, next);
+  if (!req.file) {
+    next(new BadRequest('team image must be upload'));
+  } else if (req.file) {
+    const avatarImg = `${req.protocol}://${req.get(
+      'host'
+    )}/api/v1/public/upload/${req.file.originalname}`;
+
+    const teamBody = {
+      ...req.body,
+      avatarImg,
+      createdBy: req.user._id,
+    };
+    _team.createOne({ ...teamBody, createdBy: req.user._id }, res, next);
+  }
 };
 
 const getTeam = async (req, res, next) => {
@@ -17,14 +31,46 @@ const getAllTeam = async (req, res, next) => {
 
 const updateTeam = async (req, res, next) => {
   _team.updateOne(
-    [{ _id: req.params.id }, { ...req.body }, { new: true }],
+    async (model) => {
+      let avatarImg = null;
+      if (req.file?.originalname) {
+        const team = await model.findById(req.params.id).lean().exec();
+        if (team.avatarImg) {
+          removeUploadFile(team.avatarImg);
+        }
+        avatarImg = `${req.protocol}://${req.get(
+          'host'
+        )}/api/v1/public/upload/${req.file.originalname}`;
+      }
+      return await model
+        .findOneAndUpdate(
+          { _id: req.params.id },
+          { ...req.body, ...(avatarImg && { avatarImg }) },
+          { new: true }
+        )
+        .lean()
+        .exec();
+    },
     res,
     next
   );
 };
 
 const removeTeam = async (req, res, next) => {
-  _team.removeOne({ _id: req.params.id }, res, next);
+  _team.removeOne(
+    async (model) => {
+      const removeTeam = await model
+        .findOneAndRemove({ _id: req.params.id })
+        .lean()
+        .exec();
+      if (removeTeam.avatarImg) {
+        removeUploadFile(removeTeam.avatarImg);
+      }
+      return removeTeam;
+    },
+    res,
+    next
+  );
 };
 
 module.exports = {
--- src/resources/team/teamRouter.js
+++ src/resources/team/teamRouter.js
@@ -1,18 +1,23 @@
 const { Router } = require('express');
 const controllers = require('./teamControllers');
 const { protect } = require('../../middleware/auth');
-
+const { upload, uploadFile } = require('../../middleware/upload');
 const router = Router();
 
 router
   .route('/')
   .get(controllers.getAllTeam)
-  .post(protect, controllers.createTeam);
+  .post(
+    protect,
+    upload.single('avatarImg'),
+    uploadFile,
+    controllers.createTeam
+  );
 
 router
   .route('/:id')
   .get(controllers.getTeam)
-  .put(protect, controllers.updateTeam)
+  .put(protect, upload.single('avatarImg'), uploadFile, controllers.updateTeam)
   .delete(protect, controllers.removeTeam);
 
 module.exports = router;
