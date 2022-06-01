8490482ffd558011fb9d17e6d095c92568338bd2
dhimanroyit <dhimanroyit@gmail.com>
2022-06-01 09:45:47 +0600

    delete upload file

--- src/index.js
+++ src/index.js
@@ -52,4 +52,6 @@ const server = http.createServer(app);
 dbConnect();
 const PORT = process.env.PORT || 5000;
 
-server.listen();
+server.listen(PORT, () => {
+  console.log('server running on port', PORT);
+});
